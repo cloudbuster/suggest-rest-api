4a84e926a022683749fc879748eeb4dbafaaa75e
cloudbuster <mikko.karaiste@gmail.com>
2018-03-25 23:23:34 +0300

    Entry point

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use(bodyParser.urlencoded({extended: true}))
 routes(app)
 
 let server = app.listen(3000, () => {
-  console.log("app running on port.", server.address().port);
+  console.log("App running on port.", server.address().port);
 })
 
